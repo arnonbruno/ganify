06c51fb7bec475e07abb2cca1b451578c1580e25
Bruno <asantos.quantum@gmail.com>
2019-10-27 15:11:29 -0300

    Making changes to package

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import pathlib
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -9,16 +9,16 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
-    name="GANify",
-    version="1.0.0",
+    name="ganify",
+    version="1.0.1",
     description="An Easy way to use GANs for data augmentation",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/arnonbruno/ganify",
     author="Arnon Bruno",
     author_email="asantos.quantum@gmail.com",
-    packages=find_packages(exclude=("tests",)),
+    packages=['model', 'discriminator', 'generator', 'utilities'],
     include_package_data=True,
-    install_requires=["Tensorflow 2.0", "Pandas",
-                      'Numpy', 'scikit-learn', 'matplotlib'],
+    install_requires=['tensorflow>=2.0.0', 'pandas>=0.25',
+                      'numpy>=1.16', 'scikit-learn>=0.21', 'matplotlib>=3.1'],
 )
