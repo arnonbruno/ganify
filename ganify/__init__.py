08f6bb5f392591489fe3f29d534be47faddeed07
Bruno <asantos.quantum@gmail.com>
2019-10-27 16:25:37 -0300

    Refactoting project

--- ganify/__init__.py
+++ ganify/__init__.py
@@ -1,4 +1,5 @@
 __version__ = '1.0.1'
+
 from ganify.generator.generator import Generator
 from ganify.discriminator.discriminator import Discriminator
 from ganify.discriminator.critic import Critic
--- ganify/setup.py
+++ ganify/setup.py
@@ -1,23 +1,20 @@
-import pathlib
-from setuptools import setup, find_packages
+import setuptools
 
 # The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.md").read_text()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 # This call to setup() does all the work
-setup(
+setuptools.setup(
     name="ganify",
     version="1.0.1",
     description="An Easy way to use GANs for data augmentation",
-    long_description=README,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/arnonbruno/ganify",
     author="Arnon Bruno",
     author_email="asantos.quantum@gmail.com",
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     include_package_data=True,
     python_requires='>=3.6',
     install_requires=['tensorflow>=2.0.0', 'pandas>=0.25',
